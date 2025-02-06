49ff8aea00b24367c2652d5c39d5fc0b0053c44d
jumpyguest <roland.almendras.salazar@gmail.com>
2025-02-06 15:33:24 +0800

    Implement bubble_sort method

---
+++ bubble-sort.rb
@@ -0,0 +1,20 @@
+def bubble_sort(array)
+  for passthrough in 1..array.length-1
+    swap_flag = false
+    for i in 0..array.length - (passthrough + 1)
+      if array[i] > array[i + 1]
+        temp = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+        swap_flag = true
+      end
+    end
+    unless swap_flag == true
+      break
+    end
+  end
+  p array
+end
+
+bubble_sort([4,3,78,2,0,2])
+# => => [0,2,2,3,4,78]
